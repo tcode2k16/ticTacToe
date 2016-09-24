9a482f4b3cb5504650e0ce6fea9f3e5e465ed2e7
tcode2k16 <tcode2k16@users.noreply.github.com>
2016-09-24 21:55:31 +0800

    Add files via upload

---
+++ img.js
@@ -0,0 +1,20 @@
+/**
+ * Created by home on 30/8/15.
+ */
+
+//function Sprite(img, x, y, width, height) {
+//    this.x = x;
+//    this.y = y;
+//    this.width = width;
+//    this.height = height;
+//}
+//
+//Sprite.prototype.draw = function(ctx, x, y) {
+//    ctx.drawImage(this.img, this.x, this.y, this.width, this.height,
+//        x, y, this.width, this,height);
+//}
+//
+//function initImages(img) {
+//    s_O = new Sprite(img, 0, 0, 150, 150);
+//    s_X = new Sprite(img, 0, 0, 150, 150);
+//}
